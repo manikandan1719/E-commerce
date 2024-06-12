e5845e7e79c54dc8418bd6035609fb79c675b424
manikandan <mknanda50@gmail.com>
2024-06-12 15:56:07 +0530

    ec

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ function CloseNavbar()
 {
     sidenav.style.left="-60%" 
 }
+
+document.getElementById('sub').addEventListener('click', function() {
+    this.innerHTML = 'Subscribed<i class="fa-solid fa-check"></i>';
+});
